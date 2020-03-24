956ae5f000d378c2e5a9533b84095fc0dcf49895
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-24 09:55:27 -0700

    added components and add funtion in App.js

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Card.css'
+
+const Card = props => {
+  return (
+
+    <div className="card border-secondary mb-3" >
+      <h5 class="card-title"> {props.emp.first_name + ' ' + props.emp.last_name}</h5>
+      <div className="card-body text-econdary">
+        <p className="card-text">Country: {props.emp.country}</p>
+        <p className="card-text">Department: {props.emp.department}</p>
+        <p className="card-text">{props.emp.email}</p>
+        <p className="card-text">{props.emp.gender}</p>
+
+      </div>
+    </div >
+  )
+}
+
+export default Card
